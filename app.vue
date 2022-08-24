e372767068620eb58692670311f7931a63b6dab9
Bruno De Angelis <brunordeangelis@gmail.com>
2022-08-24 08:27:20 -0300

    setup project, add image

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup>
+useHead({
+  meta: [
+    {
+      name: "robots",
+      content: "noindex",
+    },
+  ],
+});
+</script>
+
+<template>
+  <main class="w-full h-screen relative">
+    <img
+      src="/conductrai.jpg"
+      alt="CONDUCTR.AI teaser image"
+      class="w-full h-full object-cover"
+    />
+    <a href="https://conductr.co.uk" target="_blank">
+      <CDTRAILogo
+        class="px-8 absolute left-1/2 bottom-32 -translate-x-1/2 w-[700px] max-w-full"
+      />
+    </a>
+  </main>
+</template>
